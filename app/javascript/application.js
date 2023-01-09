edd2808084a3e2139aab40abc697ede5cb839c16
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-01-09 04:40:26 -0400

    Production build

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import {Turbo} from "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
-import  "jquery"
-import  * as M from "materialize"
-import "./materializeEvents"
+import "jquery"
+import "materialize"
 Turbo.session.drive = false
+
+
+$(document).on("turbo:load", ()=>{
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+
+    let flash = $('#flash')
+    if (flash.length > 0 ){
+        M.toast({
+                html: flash[0].textContent,
+                classes: flash[0].className.split("card-panel ")[1]
+            })
+    }
+
+})
 
