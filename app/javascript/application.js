35be592bd2db856cb02bedd2445ee258e3c81988
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-01-09 00:14:41 -0400

    Disabling Turbo default handling for links because it interferes with Materialize Sidenav functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import  "@hotwired/turbo-rails"
+import {Turbo} from "@hotwired/turbo-rails"
 import "controllers"
 import  "jquery"
-import  "materialize"
+import  * as M from "materialize"
+import "./materializeEvents"
+Turbo.session.drive = false
 
-
-$(document).on("turbo:load", ()=>{
-    $('.sidenav').sidenav();
-    $(".dropdown-trigger").dropdown();
-
-    let flash = $('#flash')
-    if (flash.length > 0){
-        M.toast({
-                html: flash[0].textContent,
-                classes: flash[0].className.split("card-panel ")[1]
-            })
-    }
-})
